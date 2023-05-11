cbd31666c8ddbb6350e02061ed845d82b134fc92
Rob Howdle <robhowdle94@gmail.com>
2023-05-11 14:14:14 +0100

    Initial Commit - Setup of WP Sage 10 and Acorn

---
+++ wp-content/cache/acorn/framework/views/4d4458a0344c6d370bf252de471df3faeee82c3b.php
@@ -0,0 +1,4 @@
+<footer class="content-info">
+  <?php (dynamic_sidebar('sidebar-footer')); ?>
+</footer>
+<?php /**PATH /Applications/XAMPP/xamppfiles/htdocs/portfolio/wp-content/themes/portfolio/resources/views/sections/footer.blade.php ENDPATH**/ ?>
---
+++ wp-content/cache/acorn/framework/views/d5fc1dcec2e036eda1efd22895b6501585342c21.php
@@ -0,0 +1,2 @@
+<?php (dynamic_sidebar('sidebar-primary')); ?>
+<?php /**PATH /Applications/XAMPP/xamppfiles/htdocs/portfolio/wp-content/themes/portfolio/resources/views/sections/sidebar.blade.php ENDPATH**/ ?>
